b88e1aafce193ef3a54d25b445e23806fb931e6b
Hardik Vala <hardikvala24@gmail.com>
2016-06-16 23:43:33 -0400

    Added some doc to Jewelry

--- src/problems/Jewelry.java
+++ src/problems/Jewelry.java
@@ -1,6 +1,13 @@
 package problems;
 
 
+/**
+ * The description and specifications of the <i>Jewelry</i> problem are provided here:
+ * 
+ * https://community.topcoder.com/stat?c=problem_statement&pm=1166&rd=4705
+ * 
+ * @author Hardik Vala
+ */
 public class Jewelry {
 
 	public static long howMany (int[] values) {
