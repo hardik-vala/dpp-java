52bf047403e25b9849b28593015f92eb07060af2
Hardik Vala <hardikvala24@gmail.com>
2016-06-26 23:10:05 -0400

    Remastered ChessMetric

--- src/main/java/problems/ChessMetric.java
+++ src/main/java/problems/ChessMetric.java
@@ -4,20 +4,56 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
+
 /**
- * The description and specifications of the <i>ChessMetric</i> problem are outlined here:
- * 
- * http://community.topcoder.com/stat?c=problem_statement&pm=1592&rd=4482
+ * The description and specifications of the <i>ChessMetric</i> problem are outlined
+ * <a href="http://community.topcoder.com/stat?c=problem_statement&pm=1592&rd=4482">here</a>.
  * 
  * @author Hardik Vala
  */
 public class ChessMetric {
 
-	public static long howMany (int size, int[] start, int[] end, int numMoves) {
-		long[][] howMany = new long[size][size];
-		
+	/** Size of the board (size x size chess board). */
+	private int size;
+	/** Starting (x, y) coordinates of kingknight. */
+	private int[] start;
+	/** Ending (x, y) coordinates of kingknight. */
+	private int[] end;
+	/** # of kingknight moves. */
+	private int numMoves;
+
+	/** The value at (i, j) is the # of distinct ways to each coordinates (i, j) from the start
+	 * position. */
+	private long[][] numWays;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param size - Size of the board.
+	 * @param start - Start position.
+	 * @param end - End position.
+	 * @param numMoves - # of moves.
+	 */
+	public ChessMetric(int size, int[] start, int[] end, int numMoves) {
+		this.size = size;
+		this.start = start;
+		this.end = end;
+		this.numMoves = numMoves;
+
+		this.numWays = new long[size][size];
+	}
+
+	/**
+	 * Counts the # of distinct ways to move the kingknight from the start to the end position using
+	 * exactly the given # of movies.
+	 *
+	 * @return # of distinct ways.
+	 */
+	public long count() {
+		int numMoves = this.numMoves;
+
 		Queue<int[]> frontierCoords = new LinkedList<int[]>();
-		frontierCoords.add(start);
+		frontierCoords.add(this.start);
 		
 		int[] coord;
 		while (numMoves > 0) {
@@ -25,97 +61,97 @@ public class ChessMetric {
 			
 			for (coord = it.next(); it.hasNext(); it.remove()) {
 				boolean canAccLeftCoord = coord[0] > 0;
-				boolean canAccBotCoord = coord[1] < (size - 1);
-				boolean canAccRightCoord = coord[0] < (size - 1);
+				boolean canAccBotCoord = coord[1] < (this.size - 1);
+				boolean canAccRightCoord = coord[0] < (this.size - 1);
 				boolean canAccTopCoord = coord[1] > 0;
 				
 				if (canAccLeftCoord) {
-					howMany[coord[0] - 1][coord[1]]++;
+					this.numWays[coord[0] - 1][coord[1]]++;
 					int[] coordToAdd = {coord[0] - 1, coord[1]};
 					frontierCoords.add(coordToAdd);
 				}
 				if (canAccBotCoord) {
-					howMany[coord[0]][coord[1] + 1]++;
+					this.numWays[coord[0]][coord[1] + 1]++;
 					int[] coordToAdd = {coord[0], coord[1] + 1};
 					frontierCoords.add(coordToAdd);
 				}
 				if (canAccRightCoord) {
-					howMany[coord[0] + 1][coord[1]]++;
+					this.numWays[coord[0] + 1][coord[1]]++;
 					int[] coordToAdd = {coord[0] + 1, coord[1]};
 					frontierCoords.add(coordToAdd);
 				}
 				if (canAccTopCoord) {
-					howMany[coord[0]][coord[1] - 1]++;
+					this.numWays[coord[0]][coord[1] - 1]++;
 					int[] coordToAdd = {coord[0], coord[1] - 1};
 					frontierCoords.add(coordToAdd);
 				}
 				
 				if (canAccLeftCoord && canAccBotCoord) {
-					howMany[coord[0] - 1][coord[1] + 1]++;
+					this.numWays[coord[0] - 1][coord[1] + 1]++;
 					int[] coordToAdd = {coord[0] - 1, coord[1] + 1};
 					frontierCoords.add(coordToAdd);
 				}
 				if (canAccBotCoord && canAccRightCoord) {
-					howMany[coord[0] + 1][coord[1] + 1]++;
+					this.numWays[coord[0] + 1][coord[1] + 1]++;
 					int[] coordToAdd = {coord[0] + 1, coord[1] + 1};
 					frontierCoords.add(coordToAdd);
 				}
 				if (canAccRightCoord && canAccTopCoord) {
-					howMany[coord[0] + 1][coord[1] - 1]++;
+					this.numWays[coord[0] + 1][coord[1] - 1]++;
 					int[] coordToAdd = {coord[0] + 1, coord[1] - 1};
 					frontierCoords.add(coordToAdd);
 				}
 				if (canAccTopCoord && canAccLeftCoord) {
-					howMany[coord[0] - 1][coord[1] - 1]++;
+					this.numWays[coord[0] - 1][coord[1] - 1]++;
 					int[] coordToAdd = {coord[0] - 1, coord[1] - 1};
 					frontierCoords.add(coordToAdd);
 				}
 				
 				boolean canAccLeftLeftCoord = coord[0] > 1;
-				boolean canAccBotBotCoord = coord[1] < (size - 2);
-				boolean canAccRightRightCoord = coord[0] < (size - 2);
+				boolean canAccBotBotCoord = coord[1] < (this.size - 2);
+				boolean canAccRightRightCoord = coord[0] < (this.size - 2);
 				boolean canAccTopTopCoord = coord[1] > 1;
 				
 				if (canAccLeftLeftCoord && canAccTopCoord) {
-					howMany[coord[0] - 2][coord[1] - 1]++;
+					this.numWays[coord[0] - 2][coord[1] - 1]++;
 					int[] coordToAdd = {coord[0] - 2, coord[1] - 1};
 					frontierCoords.add(coordToAdd);
 				}
 				if (canAccLeftCoord && canAccTopTopCoord) {
-					howMany[coord[0] - 1][coord[1] - 2]++;
+					this.numWays[coord[0] - 1][coord[1] - 2]++;
 					int[] coordToAdd = {coord[0] - 1, coord[1] - 2};
 					frontierCoords.add(coordToAdd);
 				}
 				
 				if (canAccRightCoord && canAccTopTopCoord) {
-					howMany[coord[0] + 1][coord[1] - 2]++;
+					this.numWays[coord[0] + 1][coord[1] - 2]++;
 					int[] coordToAdd = {coord[0] + 1, coord[1] - 2};
 					frontierCoords.add(coordToAdd);
 				}
 				if (canAccRightRightCoord && canAccTopCoord) {
-					howMany[coord[0] + 2][coord[1] - 1]++;
+					this.numWays[coord[0] + 2][coord[1] - 1]++;
 					int[] coordToAdd = {coord[0] + 2, coord[1] - 1};
 					frontierCoords.add(coordToAdd);
 				}
 				
 				if (canAccRightRightCoord && canAccBotCoord) {
-					howMany[coord[0] + 2][coord[1] + 1]++;
+					this.numWays[coord[0] + 2][coord[1] + 1]++;
 					int[] coordToAdd = {coord[0] + 2, coord[1] + 1};
 					frontierCoords.add(coordToAdd);
 				}
 				if (canAccRightCoord && canAccBotBotCoord) {
-					howMany[coord[0] + 1][coord[1] + 2]++;
+					this.numWays[coord[0] + 1][coord[1] + 2]++;
 					int[] coordToAdd = {coord[0] + 1, coord[1] + 2};
 					frontierCoords.add(coordToAdd);
 				}
 				
 				if (canAccLeftCoord && canAccBotBotCoord) {
-					howMany[coord[0] - 1][coord[1] + 2]++;
+					this.numWays[coord[0] - 1][coord[1] + 2]++;
 					int[] coordToAdd = {coord[0] - 1, coord[1] + 2};
 					frontierCoords.add(coordToAdd);
 				}
 				if (canAccLeftLeftCoord && canAccBotCoord) {
-					howMany[coord[0] - 2][coord[1] + 1]++;
+					this.numWays[coord[0] - 2][coord[1] + 1]++;
 					int[] coordToAdd = {coord[0] - 2, coord[1] + 1};
 					frontierCoords.add(coordToAdd);
 				}
@@ -124,7 +160,7 @@ public class ChessMetric {
 			numMoves--;
 		}
 		
-		return howMany[end[0]][end[1]];
+		return this.numWays[this.end[0]][this.end[1]];
 	}
 	
 }
--- src/test/java/ChessMetricTest.java
+++ src/test/java/ChessMetricTest.java
@@ -1,55 +1,39 @@
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 import problems.ChessMetric;
 
-import java.util.ArrayList;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 
 public class ChessMetricTest {
-
-	private static final int[] sizes = {3, 3, 3, 3, 100};
-	private static final ArrayList<int[]> starts = new ArrayList<int[]>();
-	private static final ArrayList<int[]> ends = new ArrayList<int[]>();
-	private static final int[] numMovess = {1, 1, 1, 2, 50};
 	
-	private static final long[] howManys = {1, 1, 0, 5, Long.parseLong("243097320072600")};
-	
-	@BeforeClass
-	public static void setUpBeforeClass() {
-		int[] s1 = {0, 0};
-		starts.add(s1);
-		int[] e1 = {1, 0};
-		ends.add(e1);
-		
-		int[] s2 = {0, 0};
-		starts.add(s2);
-		int[] e2 = {1, 2};
-		ends.add(e2);
-		
-		int[] s3 = {0, 0};
-		starts.add(s3);
-		int[] e3 = {2, 2};
-		ends.add(e3);
-		
-		int[] s4 = {0, 0};
-		starts.add(s4);
-		int[] e4 = {0, 0};
-		ends.add(e4);
-		
-		int[] s5 = {0, 0};
-		starts.add(s5);
-		int[] e5 = {0, 99};
-		ends.add(e5);
+	@Test
+	public void testAdjacentStartAndEnd() {
+		ChessMetric cm = new ChessMetric(3, new int[] {0, 0}, new int[] {1, 0}, 1);
+		assertEquals(1, cm.count());
 	}
 
 	@Test
-	public void testHowMany() {
-		for (int i = 0; i < sizes.length; i++) {
-			assertTrue(ChessMetric.howMany(sizes[i], starts.get(i), ends.get(i), numMovess[i]) == howManys[i]);
-		}
+	public void testSingleLShapedMove() {
+		ChessMetric cm = new ChessMetric(3, new int[] {0, 0}, new int[] {1, 2}, 1);
+		assertEquals(1, cm.count());
+	}
+
+	@Test
+	public void testStartAndEndTooFar() {
+		ChessMetric cm = new ChessMetric(3, new int[] {0, 0}, new int[] {2, 2}, 1);
+		assertEquals(0, cm.count());
+	}
+
+	@Test
+	public void testLeavingAndReturningToCorner() {
+		ChessMetric cm = new ChessMetric(3, new int[] {0, 0}, new int[] {0, 0}, 2);
+		assertEquals(5, cm.count());
+	}
+
+	@Test
+	public void testBigBoard() {
+		ChessMetric cm = new ChessMetric(100, new int[] {0, 0}, new int[] {0, 99}, 50);
+		assertEquals(243097320072600L, cm.count());
 	}
 
 }
