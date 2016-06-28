12529bfeae46a24a451ecf09b47653246707112f
Hardik Vala <hardikvala24@gmail.com>
2016-06-28 16:37:40 -0400

    Remastered FlowerGarden

--- src/main/java/problems/FlowerGarden.java
+++ src/main/java/problems/FlowerGarden.java
@@ -3,63 +3,43 @@ package problems;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.TreeSet;
 
 /**
- * The description and specifications of the <i>Flower Garden</i> problem are outlined here:
- * 
- * http://community.topcoder.com/stat?c=problem_statement&pm=1918&rd=5006
+ * The description and specifications of the <i>Flower Garden</i> problem are outlined
+ * <a href="http://community.topcoder.com/stat?c=problem_statement&pm=1918&rd=5006">here</a>.
  * 
  * @author Hardik Vala
  */
 public class FlowerGarden {
 	
-	public static int[] getOrdering (int[] h, int[] b, int[] w) {
-		int[] ordering = new int[h.length];
-		
-		LinkedList<TreeSet<Flower>> subOrderings = new LinkedList<TreeSet<Flower>>();
-		
-		for (int i = 0; i < h.length; i++) {
-			boolean foundOrdering = false;
-			
-			for (TreeSet<Flower> so : subOrderings) {
-				for (Flower f : so) {
-					if ((f.bloom <= w[i]) && (b[i] <= f.wilt)) {
-						foundOrdering = true;
-						break;
-					}
-				}
-				if (foundOrdering) {
-					so.add(new Flower(h[i], b[i], w[i]));
-					break;
-				}
-			}
-			
-			if (!foundOrdering) {
-				TreeSet<Flower> newSubOrdering = new TreeSet<Flower>();
-				newSubOrdering.add(new Flower(h[i], b[i], w[i]));
-				subOrderings.add(newSubOrdering);
-			}
-		}
-		
-		Collections.sort(subOrderings, new Comparator<TreeSet<Flower>>() {
-			@Override
-			public int compare (TreeSet<Flower> so1, TreeSet<Flower> so2) {
-				return so2.first().height - so1.first().height;
-			}
-		});
-		
-		int i = 0;
-		for (TreeSet<Flower> so : subOrderings) {
-			for (Flower f : so) {
-				ordering[i] = f.height;
-				i++;
-			}
-		}
-		
-		return ordering;
+	/** Heights of the flowers. */
+	private int[] height;
+	/** The morning each type of flower springs. */
+	private int[] bloom;
+	/** The evening that each type of flower dies. */
+	private int[] wilt;
+
+	/** Tracks elements of height in the order flowers should be planted. */
+	private int[] ordering;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param height - Heights of flowers.
+	 * @param bloom - Mornings each flower blooms.
+	 * @param wilt - Evens each flower dies.
+	 */
+	public FlowerGarden(int[] height, int[] bloom, int[] wilt) {
+		this.height = height;
+		this.bloom = bloom;
+		this.wilt = wilt;
+
+		this.ordering = new int[height.length];
 	}
-	
+
+	// Convenience class for representing a flower.
 	private static class Flower implements Comparable<Flower>{
 		
 		private int height;
@@ -78,4 +58,55 @@ public class FlowerGarden {
 		}
 		
 	}
+
+	/**
+	 * Determines the optimal ordering in which flowers should be planted.
+	 *
+	 * @return Elements of height in the order flowers should be planted.
+	 */
+	public int[] det() {
+		List<TreeSet<Flower>> subOrderings = new LinkedList<TreeSet<Flower>>();
+		
+		for (int i = 0; i < this.height.length; i++) {
+			boolean foundOrdering = false;
+			
+			for (TreeSet<Flower> so : subOrderings) {
+				for (Flower f : so) {
+					if ((f.bloom <= this.wilt[i]) && (this.bloom[i] <= f.wilt)) {
+						foundOrdering = true;
+						break;
+					}
+				}
+
+				if (foundOrdering) {
+					so.add(new Flower(this.height[i], this.bloom[i], this.wilt[i]));
+					break;
+				}
+			}
+			
+			if (!foundOrdering) {
+				TreeSet<Flower> newSubOrdering = new TreeSet<Flower>();
+				newSubOrdering.add(new Flower(this.height[i], this.bloom[i], this.wilt[i]));
+				subOrderings.add(newSubOrdering);
+			}
+		}
+		
+		Collections.sort(subOrderings, new Comparator<TreeSet<Flower>>() {
+			@Override
+			public int compare (TreeSet<Flower> so1, TreeSet<Flower> so2) {
+				return so2.first().height - so1.first().height;
+			}
+		});
+		
+		int i = 0;
+		for (TreeSet<Flower> so : subOrderings) {
+			for (Flower f : so) {
+				this.ordering[i] = f.height;
+				i++;
+			}
+		}
+		
+		return this.ordering;
+	}
+	
 }
--- src/test/java/FlowerGardenTest.java
+++ src/test/java/FlowerGardenTest.java
@@ -1,105 +1,81 @@
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 import problems.FlowerGarden;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 
 public class FlowerGardenTest {
-
-	private static final ArrayList<int[]> heights = new ArrayList<int[]>();
-	private static final ArrayList<int[]> blooms = new ArrayList<int[]>();
-	private static final ArrayList<int[]> wilts = new ArrayList<int[]>();
-	private static final ArrayList<int[]> orderings = new ArrayList<int[]>();
 	
-	@BeforeClass
-	public static void setUp () {
-		int[] h1 = {5,4,3,2,1};
-		int[] b1 = {1,1,1,1,1};
-		int[] w1 = {365,365,365,365,365};
-		int[] o1 = {1,2,3,4,5};
-		
-		heights.add(h1);
-		blooms.add(b1);
-		wilts.add(w1);
-		orderings.add(o1);
-		
-		int[] h2 = {5,4,3,2,1};
-		int[] b2 = {1,5,10,15,20};
-		int[] w2 = {4,9,14,19,24};
-		int[] o2 = {5,4,3,2,1};
-		
-		heights.add(h2);
-		blooms.add(b2);
-		wilts.add(w2);
-		orderings.add(o2);
-		
-		int[] h3 = {5,4,3,2,1};
-		int[] b3 = {1,5,10,15,20};
-		int[] w3 = {5,10,15,20,25};
-		int[] o3 = {1,2,3,4,5};
-		
-		heights.add(h3);
-		blooms.add(b3);
-		wilts.add(w3);
-		orderings.add(o3);
-		
-		int[] h4 = {5,4,3,2,1};
-		int[] b4 = {1,5,10,15,20};
-		int[] w4 = {5,10,14,20,25};
-		int[] o4 = {3,4,5,1,2};
-		
-		heights.add(h4);
-		blooms.add(b4);
-		wilts.add(w4);
-		orderings.add(o4);
-		
-		int[] h5 = {1,2,3,4,5,6};
-		int[] b5 = {1,3,1,3,1,3};
-		int[] w5 = {2,4,2,4,2,4};
-		int[] o5 = {2,4,6,1,3,5};
-		
-		heights.add(h5);
-		blooms.add(b5);
-		wilts.add(w5);
-		orderings.add(o5);
-		
-		int[] h6 = {3,2,5,4};
-		int[] b6 = {1,2,11,10};
-		int[] w6 = {4,3,12,13};
-		int[] o6 = {4,5,2,3};
-		
-		heights.add(h6);
-		blooms.add(b6);
-		wilts.add(w6);
-		orderings.add(o6);
+	// These flowers all bloom on January 1st and wilt on December 31st. Since they all may block
+	// each other, they must be ordered from shortest to tallest.
+	@Test
+	public void testSameBloomAndWilts() {
+		int[] height = new int[] {5, 4, 3, 2, 1};
+		int[] bloom = new int[] {1, 1, 1, 1, 1};
+		int[] wilt = new int[] {365, 365, 365, 365, 365};
+
+		FlowerGarden fg = new FlowerGarden(height, bloom, wilt);
+		assertArrayEquals(new int[] {1, 2, 3, 4, 5}, fg.det());
+	}
+
+	// The same set of flowers now bloom all at separate times. Since they will never block each
+	// other, they can be ordered from tallest to shortest to get the tallest ones as far forward as
+	// possible.
+	@Test
+	public void testNoBlocking() {
+		int[] height = new int[] {5, 4, 3, 2, 1};
+		int[] bloom = new int[] {1, 5, 10, 15, 20};
+		int[] wilt = new int[] {4, 9, 14, 19, 24};
+
+		FlowerGarden fg = new FlowerGarden(height, bloom, wilt);
+		assertArrayEquals(new int[] {5, 4, 3, 2, 1}, fg.det());
+	}
+
+	// Although each flower only blocks at most one other, they all must be ordered from shortest to
+	// tallest to prevent any blocking from occurring.
+	@Test
+	public void testEachFlowerBlocksOneOtherExceptLast() {
+		int[] height = new int[] {5, 4, 3, 2, 1};
+		int[] bloom = new int[] {1, 5, 10, 15, 20};
+		int[] wilt = new int[] {5, 10, 15, 20, 25};
+
+		FlowerGarden fg = new FlowerGarden(height, bloom, wilt);
+		assertArrayEquals(new int[] {1, 2, 3, 4, 5}, fg.det());
+	}
+
+	// The difference here is that the third type of flower wilts one day earlier than the blooming
+	// of the fourth flower. Therefore, the flowers of height 3 can be put first, then the flowers
+	// of height 4, then height 5, and finally the flowers of height 1 and 2. Note that they could
+	// have also been ordered with height 1 first, but this does not result in the maximum possible
+	// height being first in the garden.
+	@Test
+	public void testEachFlowerBlocksAtMostOneOther() {
+		int[] height = new int[] {5, 4, 3, 2, 1};
+		int[] bloom = new int[] {1, 5, 10, 15, 20};
+		int[] wilt = new int[] {5, 10, 14, 20, 25};
+
+		FlowerGarden fg = new FlowerGarden(height, bloom, wilt);
+		assertArrayEquals(new int[] {3, 4, 5, 1, 2}, fg.det());
 	}
 
 	@Test
-	public void testGetOrderings () {
-		System.out.println("<<< TEST FLOWER GARDENS >>>\n");
-		
-		int numFlowers = heights.size();
-		
-		for (int i = 0; i < numFlowers; i++) {
-			System.out.println("Garden #" + (i + 1));
-			int[] h = heights.get(i);
-			System.out.println("\tHeights: " + Arrays.toString(h));
-			int[] b = blooms.get(i);
-			System.out.println("\tBlooms: " + Arrays.toString(b));
-			int[] w = wilts.get(i);
-			System.out.println("\tWilts: " + Arrays.toString(w));
-			int[] o = FlowerGarden.getOrdering(h, b, w);
-			System.out.print("\tOrdering: " + Arrays.toString(o));
-			
-			assertTrue(Arrays.equals(o, orderings.get(i)));
-			
-			if (i < (numFlowers - 1)) System.out.println('\n');
-		}
+	public void testAlternatingBloomAndWilts() {
+		int[] height = new int[] {1, 2, 3, 4, 5, 6};
+		int[] bloom = new int[] {1, 3, 1, 3, 1, 3};
+		int[] wilt = new int[] {2, 4, 2, 4, 2, 4};
+
+		FlowerGarden fg = new FlowerGarden(height, bloom, wilt);
+		assertArrayEquals(new int[] {2, 4, 6, 1, 3, 5}, fg.det());
+	}
+
+	@Test
+	public void testSubsumingBloomAndWiltPeriods() {
+		int[] height = new int[] {3, 2, 5, 4};
+		int[] bloom = new int[] {1, 2, 11, 10};
+		int[] wilt = new int[] {4, 3, 12, 13};
+
+		FlowerGarden fg = new FlowerGarden(height, bloom, wilt);
+		assertArrayEquals(new int[] {4, 5, 2, 3}, fg.det());
 	}
 
 }
