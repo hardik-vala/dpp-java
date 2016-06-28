391cb82b73cbc07f515686de5e219120079b428c
Hardik Vala <hardikvala24@gmail.com>
2016-06-28 16:42:09 -0400

    Added notes to ChessMetricTest

--- src/test/java/ChessMetricTest.java
+++ src/test/java/ChessMetricTest.java
@@ -6,24 +6,28 @@ import problems.ChessMetric;
 
 public class ChessMetricTest {
 	
+	// Only 1 way to get to an adjacent square in 1 move.
 	@Test
 	public void testAdjacentStartAndEnd() {
 		ChessMetric cm = new ChessMetric(3, new int[] {0, 0}, new int[] {1, 0}, 1);
 		assertEquals(1, cm.count());
 	}
 
+	// A single L-shaped move is the only way.
 	@Test
 	public void testSingleLShapedMove() {
 		ChessMetric cm = new ChessMetric(3, new int[] {0, 0}, new int[] {1, 2}, 1);
 		assertEquals(1, cm.count());
 	}
 
+	// Too far for a single move.
 	@Test
 	public void testStartAndEndTooFar() {
 		ChessMetric cm = new ChessMetric(3, new int[] {0, 0}, new int[] {2, 2}, 1);
 		assertEquals(0, cm.count());
 	}
 
+	// Must count all the ways of leaving and then returning to the corner.
 	@Test
 	public void testLeavingAndReturningToCorner() {
 		ChessMetric cm = new ChessMetric(3, new int[] {0, 0}, new int[] {0, 0}, 2);
