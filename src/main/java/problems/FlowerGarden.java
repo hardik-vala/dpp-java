ae1471ed071b47c084a21ceaf7ae9d7dec10bb15
Hardik Vala <hardikvala24@gmail.com>
2016-06-30 19:07:14 -0400

    Fixed some formatting in FlowerGarden

--- src/main/java/problems/FlowerGarden.java
+++ src/main/java/problems/FlowerGarden.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.TreeSet;
 
+
 /**
  * The description and specifications of the <i>Flower Garden</i> problem are outlined
  * <a href="http://community.topcoder.com/stat?c=problem_statement&pm=1918&rd=5006">here</a>.
