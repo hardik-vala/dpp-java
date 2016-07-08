c3ffc6711f46cb264e73cbc7ce8aafb436a24abd
Hardik Vala <hardikvala24@gmail.com>
2016-07-07 23:29:39 -0400

    Cleaned-up CoinChange doc

--- src/main/java/problems/CoinChange.java
+++ src/main/java/problems/CoinChange.java
@@ -32,22 +32,6 @@ public class CoinChange {
 		this.numWays = new int[this.N + 1][this.C.length];
 	}
 
-	/*
-def sol(N, M, C):
-    m = [[0 for _ in range(M)] for _ in range(N + 1)]
-    
-    for j in range(M):
-        m[0][j] = 1
-    
-    for i in xrange(1, N + 1):
-        for j in xrange(M):
-            x = m[i - C[j]][j] if i - C[j] >= 0 else 0
-            y = m[i][j - 1] if j >= 1 else 0
-            m[i][j] = x + y
-        
-    return m[N][M - 1]
-*/
-
 	/**
 	 * Calculates the number of ways you can make change for N out of the coins
 	 * C.
