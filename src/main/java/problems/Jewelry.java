ea275433bb06dbfcb46a61042fbc32c22f6f86b0
Hardik Vala <hardikvala24@gmail.com>
2016-07-17 23:43:23 -0400

    Partially implemented Jewelry

--- src/main/java/problems/Jewelry.java
+++ src/main/java/problems/Jewelry.java
@@ -1,17 +1,79 @@
 package problems;
 
+import java.util.Arrays;
+
 
 /**
- * The description and specifications of the <i>Jewelry</i> problem are provided here:
- * 
- * https://community.topcoder.com/stat?c=problem_statement&pm=1166&rd=4705
+ * The description and specifications of the <i>Flower Garden</i> problem are outlined
+ * <a href="https://community.topcoder.com/stat?c=problem_statement&pm=1166&rd=4705">here</a>.
  * 
  * @author Hardik Vala
  */
 public class Jewelry {
 
-	public static long howMany (int[] values) {
-		
-		return 0;
+	/** Value of each piece of jewelry. */
+	int[] jewelry;
+
+	/** Index i stores the # of ways of allocating Bob or Frank's jewelry, respectively, to sum to
+	 * i. */
+	// long[] numBobsWays;
+	// long[] numFranksWays;
+
+
+	/**
+	 * Constructor.
+	 *
+	 * @param jewelry - Value of each piece of jewelry.
+	 */
+	public Jewelry(int[] jewelry) {
+		if (jewelry.length <= 1)
+			throw new IllegalArgumentException("Must have at least 2 pieces of jewelry.");
+
+		this.jewelry = jewelry;
+
+		// int sum = 0;
+		// for (int i = 0; i < jewelry.length; i++) sum += jewelry[i];
+
+		// this.numBobsWays = new long[sum + 1];
+		// this.numFranksWays = new long[sum + 1];
 	}
+
+	public long count() {
+		int[] sortedJewelry = Arrays.copyOf(this.jewelry, this.jewelry.length);
+		Arrays.sort(sortedJewelry);
+		int numJewelry = sortedJewelry.length;
+		int sum = Arrays.stream(sortedJewelry).sum();
+
+		long[] accNumBobsWays = new long[sum + 1];
+		accNumBobsWays[0] = 1;
+
+		long numWays = 0L;
+		for (int i = 0; i < numJewelry - 1; i++) {
+			long[] newNumBobsWays = new long[sum + 1];
+			for (int k = sum; k >= sortedJewelry[i]; k--)
+				newNumBobsWays[k] += accNumBobsWays[k - sortedJewelry[i]];
+
+			long[] numFranksWays = new long[sum + 1];
+			numFranksWays[0] = 1;
+			for (int j = i + 1; j < numJewelry; j++) {
+				for (int k = sum; k >= sortedJewelry[j]; k--)
+					numFranksWays[k] += numFranksWays[k - sortedJewelry[j]];
+			}
+
+			// System.out.println(i);
+			// System.out.println(Arrays.toString(accNumBobsWays));
+			// System.out.println(Arrays.toString(newNumBobsWays));
+			// System.out.println(Arrays.toString(numFranksWays));
+
+			for (int k = 1; k <= sum; k++) {
+				numWays += newNumBobsWays[k] * numFranksWays[k];
+				accNumBobsWays[k] += newNumBobsWays[k];
+			}
+
+			// System.out.println(numWays);
+		}
+
+		return numWays;
+	}
+
 }
---
+++ src/test/java/JewelryTest.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import problems.Jewelry;
+
+
+public class JewelryTest {
+	
+	// Valid allocations:
+	// Bob              Frank
+	// 1,2                3
+	// 2,3                5
+	// 1,3                4
+	// 1,4                5
+	@Test
+	public void testSmallJewlerySet() {
+		Jewelry j = new Jewelry(new int[] {1, 2, 3, 4, 5});
+		assertEquals(4L, j.count());
+	}
+
+	// Valid allocations are:
+	//	 Bob       		Frank
+	//	 1,2		      3
+	//   1,3       		  4
+	//   1,4		      5  (first 5)
+	//   1,4         	  5  (second 5)
+	//   2,3 		      5  (first 5)
+	//   2,3         	  5  (second 5)
+	//    5  (first 5)	  5  (second 5)
+	//    5  (second 5)	  5  (first 5)
+	// 1,2,3,4       	 5,5
+	// Note that each '5' is a different piece of jewelry and needs to be accounted for separately.
+	// There are 9 legal ways of allocating the jewelry to Bob and Frank given the policy.
+	@Test
+	public void testSmallJewlerySetWithDup() {
+		Jewelry j = new Jewelry(new int[] {1, 2, 5, 3, 4, 5});
+		assertEquals(9L, j.count());
+	}
+
+	@Test
+	public void testBiggerJewlerySetWithDups() {
+		Jewelry j = new Jewelry(new int[] {7, 7, 8, 9, 10, 11, 1, 2, 2, 3, 4, 5, 6});
+		assertEquals(607L, j.count());
+	}
+
+	@Test
+	public void testBiggerJewlerySetWithAllDups() {
+		int[] jewelry = new int[] {
+			1000, 1000, 1000, 1000, 1000,
+			1000, 1000, 1000, 1000, 1000,
+			1000, 1000, 1000, 1000, 1000,
+			1000, 1000, 1000, 1000, 1000,
+			1000, 1000, 1000, 1000, 1000,
+			1000, 1000, 1000, 1000, 1000
+		};
+		Jewelry j = new Jewelry(jewelry);
+		assertEquals(18252025766940L, j.count());
+	}
+
+	@Test
+	public void testNoPossibleAllocations() {
+		int[] jewelry = new int[] {123, 217, 661, 678, 796, 964, 54, 111, 417, 526, 917, 923};
+		Jewelry j = new Jewelry(jewelry);
+		assertEquals(0L, j.count());
+	}
+
+}
