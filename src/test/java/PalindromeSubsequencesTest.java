a68c654d30fa3828f3b7e3cb47f194a89cfae177
Hardik Vala <hardikvala24@gmail.com>
2016-07-19 23:43:41 -0400

    Added PalindromeSubsequencesTest.java

---
+++ src/test/java/PalindromeSubsequencesTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import problems.PalindromeSubsequences;
+
+
+public class PalindromeSubsequencesTest {
+	
+	@Test
+	public void testSmallSeq() {
+		List<String> seq = Arrays.asList(new String[] {"a", "b", "c", "a"});
+		PalindromeSubsequences ps = new PalindromeSubsequences(seq);
+		assertEquals(7, ps.count());
+	}
+
+	@Test
+	public void testSmallPalindromeSeq() {
+		List<String> seq = Arrays.asList(new String[] {"a", "b", "a", "b", "a"});
+		PalindromeSubsequences ps = new PalindromeSubsequences(seq);
+		assertEquals(17, ps.count());
+	}
+
+}
