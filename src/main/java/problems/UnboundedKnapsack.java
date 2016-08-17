f743545cc6ddeacffce242c8860a32a9ca8d518b
Hardik Vala <hardikvala24@gmail.com>
2016-08-17 00:09:37 -0400

    Refactored and add checks in UnboundedKnapsack

--- src/main/java/problems/UnboundedKnapsack.java
+++ src/main/java/problems/UnboundedKnapsack.java
@@ -25,11 +25,19 @@ public class UnboundedKnapsack {
      * @param values - Item values.
      * @param weights - Item weights.
 	 * @param capacity - Capacity.
+     * @precondition values[i], weights[i], capacity >= 0, for all i.
+     * @throws IllegalArgumentException if an item has zero weight but non-zero value.
 	 */
 	public UnboundedKnapsack(int[] values, int[] weights, int capacity) {
         this.values = values;
         this.weights = weights;
 		this.capacity = capacity;
+
+        for (int i = 0; i < this.values.length; i++) {
+            if (this.values[i] > 0 && this.weights[i] == 0)
+                throw new IllegalArgumentException("Encountered item with zero weight and non-zero "
+                    + "value.");
+        }
 
 		this.maxTotValues = new int[capacity + 1];
 	}
@@ -39,7 +47,7 @@ public class UnboundedKnapsack {
 	 *
 	 * @return Max. value.
 	 */
-	public long calc() {
+	public int calc() {
 		for (int subCapacity = 0; subCapacity <= this.capacity; subCapacity++) {
 			int maxTotValue = 0;
             for (int item = 0; item < this.values.length; item++) {
