3d3b5ba928a271a34ba7930e062a7a8b46d67ecf
Hardik Vala <hardikvala24@gmail.com>
2016-08-25 21:34:20 -0400

    Removed raw types from PalindromeSubsequencesTest

--- src/test/java/PalindromeSubsequencesTest.java
+++ src/test/java/PalindromeSubsequencesTest.java
@@ -12,14 +12,14 @@ public class PalindromeSubsequencesTest {
 	@Test
 	public void testSmallSeq() {
 		List<String> seq = Arrays.asList(new String[] {"a", "b", "c", "a"});
-		PalindromeSubsequences ps = new PalindromeSubsequences(seq);
+		PalindromeSubsequences<String> ps = new PalindromeSubsequences<>(seq);
 		assertEquals(7, ps.count());
 	}
 
 	@Test
 	public void testSmallPalindromeSeq() {
 		List<String> seq = Arrays.asList(new String[] {"a", "b", "a", "b", "a"});
-		PalindromeSubsequences ps = new PalindromeSubsequences(seq);
+		PalindromeSubsequences<String> ps = new PalindromeSubsequences<>(seq);
 		assertEquals(17, ps.count());
 	}
 
